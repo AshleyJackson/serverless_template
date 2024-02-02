efb3804beea4da31b362908e36323d93b9d7070c
Ashley Jackson <ajack_94@hotmail.com>
2024-02-03 00:03:56 +1000

    Functional template

---
+++ src/handler.ts
@@ -0,0 +1,12 @@
+// serverless.yml index.ts for AWS Lambda function, handler.hello
+import type { Handler, Context } from 'aws-lambda';
+
+export const hello: Handler = async (event: any, context: Context) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
+      input: event,
+    }, null, 2),
+  };
+};
